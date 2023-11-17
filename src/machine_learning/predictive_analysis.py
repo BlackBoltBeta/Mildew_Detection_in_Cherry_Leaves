3465b0c291a9d696fee8569cf1ff285a51c9f87a
Nicolas Saavedra <h.n.saavedra@gmail.com>
2023-11-17 19:11:46 +0100

    IDE issues

--- src/machine_learning/predictive_analysis.py
+++ src/machine_learning/predictive_analysis.py
@@ -38,7 +38,7 @@ def resize_input_image(img, version):
     Reshape image to average image size
     """
     image_shape = load_pkl_file(file_path=f"outputs/{version}/image_shape.pkl")
-    img_resized = img.resize((image_shape[1], image_shape[0]), Image.ANTIALIAS)
+    img_resized = img.resize((image_shape[1], image_shape[0]), Image.LANCZOS)
     my_image = np.expand_dims(img_resized, axis=0)/255
 
     return my_image
@@ -49,13 +49,13 @@ def load_model_and_predict(my_image, version):
     Load and perform ML prediction over live images
     """
 
-    model = load_model(f"outputs/{version}/mildew_detector_model.h5")
+    model = load_model(f"outputs/{version}/mildew_model.h5")
 
     pred_proba = model.predict(my_image)[0, 0]
 
     target_map = {v: k for k, v in {'Healthy': 0, 'Infected': 1}.items()}
     pred_class = target_map[pred_proba > 0.5]
-    if pred_class == target_map[0]:
+    if pred_class == target_map[1]:
         pred_proba = 1 - pred_proba
 
     st.write(
